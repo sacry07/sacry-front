88faf5909ae329bbc85112f67c820a5014d85b8f
Klein Suarez <yahir.kleins@gmail.com>
2024-09-22 20:48:28 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <section>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to Sacry</h1>
+          <p></p>
+        </header>
+      </div>
+      <footer className="App-footer">
+        <p> 2024 Sacry. All rights reserved.</p>
+      </footer>
+    </section>
+  )
+}
+
+export default App
