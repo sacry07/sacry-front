0de4630c5a2416dc32db8a59c78d5ac83f61dad6
Yahir Klein Quintero Suarez <50756668+KleinSuarez@users.noreply.github.com>
2024-12-18 23:10:53 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,10 @@ function App() {
       <div className="App">
         <header className="App-header">
           <h1>Welcome to Sacry</h1>
+          <h2>Pagina en progreso </h2>
           <p>Te amo ❤️</p>
           <h4>Ribhelly</h4>
+          <h5>¿Te casarias conmigo? 💍 </h5>
         </header>
       </div>
       <footer className="App-footer">
